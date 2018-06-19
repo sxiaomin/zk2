2543e25ad657385a381341517a3c04b1c0888c0b
sxiaomin <m1371786385151_1@163.com>
2018-06-19 21:35:57 +0800

    打包

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var clean = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
+var sequence = require('gulp-sequence');
 // 引入内置模块
 var url = require('url');
 var fs = require('fs');
@@ -23,13 +24,47 @@ gulp.task('server', function() {
                 if (pathname === '/favicon.ico') {
                     return false;
                 }
-                //  if (pathname === '/api/list') {
-                // res.end(json);
-                //  } else {
-                pathname = pathname === '/' ? '/index.html' : pathname;
-                res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)));
-                // }
+                if (pathname === '/api/list') {
+                    res.end(JSON.stringify(json));
+                } else {
+                    pathname = pathname === '/' ? '/index.html' : pathname;
+                    res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)));
+                }
 
             }
         }))
 });
+
+// gulp.task('watch', function() {
+//     gulp.watch('src/sass/*.scss', [])
+// })
+
+// sass编译 压缩 打包
+gulp.task('sass', function() {
+    return gulp.src('src/sass/*.scss')
+        .pipe(sass())
+        .pipe(clean())
+        .pipe(gulp.dest('build/css'))
+});
+
+// // js压缩 打包
+gulp.task('uglify', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'))
+});
+gulp.task('copyJs', function() {
+    return gulp.src(['src/js/main.js', 'src/js/**/*.js', 'src/js/app/*.js'])
+        .pipe(gulp.dest('build/js'))
+});
+
+// // html
+gulp.task('htmlmin', function() {
+    return gulp.src('src/**/*.html')
+        .pipe(htmlmin())
+        .pipe(gulp.dest('build'))
+});
+
+gulp.task('build', function(cb) {
+    sequence('sass', 'uglify', 'copyJs', 'htmlmin', cb);
+})
